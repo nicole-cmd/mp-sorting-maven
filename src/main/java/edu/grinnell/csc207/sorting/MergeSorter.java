c318810acd5b51ba764844aec18e80ac711b2ab1
gorrelln <gorrelln@grinnell.edu>
2024-12-20 09:27:29 -0600

    fixed merge sorter

--- src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
+++ src/main/java/edu/grinnell/csc207/sorting/MergeSorter.java
@@ -1,5 +1,6 @@
 package edu.grinnell.csc207.sorting;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
@@ -56,67 +57,71 @@ public class MergeSorter<T> implements Sorter<T> {
    */
   @Override
   public void sort(T[] values) {
+    try {
+      mergeSort(values);
+    } catch (Exception e) {
+      // Do nothing.
+    } // try/catch
+  } // sort(T[])
+
+  // +-----------------+-----------------------------------------------------
+  // | Private Methods |
+  // +-----------------+
+  
+  /**
+   * Sort an array using selection sort.
+   *
+   * @param values
+   *   an array to sort.
+   *
+   * @post
+   *   The array has been sorted according to some order (often
+   *   one given to the constructor).
+   * @post
+   *   For all i, 0 &lt; i &lt; values.length,
+   *     order.compare(values[i-1], values[i]) &lt;= 0
+   * @throws Exception if the array is not of an appropriate length.
+   */
+  public void mergeSort(T[] values) throws Exception {
     if (values.length <= 1) {
+      throw new Exception("Array is not long enough to sort");
+    } else if (values.length == 1) {
       return;
     } // if
 
     int middle = values.length / 2;
+    T[] secondHalf = Arrays.copyOfRange(values, middle, values.length);
 
     for (int i = 0; i < values.length; i++) {
       if (i < middle) {
         if (order.compare(values[++i], values[i]) < 0) {
-          swap(values, ++i, i);
+          Sorter.swap(values, ++i, i);
         } // if
 
-        sort(values);
+        mergeSort(values);
       } // if
 
-      sort(values);
+      mergeSort(secondHalf);
     } // for
 
-    // pointers to keep track of positions in the split array
+    // pointer to keep track of positions in the first split array
     int track1 = 0;
-    int track2 = middle;
 
     @SuppressWarnings({"unchecked"})
     T[] newArray = (T[]) new Object[values.length];
 
     for (int i = 0; i < newArray.length; i++) {
-      if (order.compare(values[track1], values[track2]) < 0) {
+      if (order.compare(values[track1], values[middle]) < 0) {
         newArray[i] = values[track1];
         track1++;
       } else {
-        newArray[i] = values[track2];
-        track2++;
+        newArray[i] = values[middle];
+        middle++;
       } // if/else
     } // for
     
-    /** to remain compatible with Sorter interface, manipulate
-     *  original array */
+    // to remain compatible with Sorter interface, manipulate
+    // original array
     values = newArray;
-  } // sort(T[])
-
-  /**
-   * Swap two values in an array.
-   * 
-   * @param arr
-   *   the array in which we're swapping.
-   * @param i1
-   *   the index of the first value to swap.
-   * @param i2
-   *   the index of the second value to swap. 
-   * 
-   * @post
-   *   The two values have switched places (i1 is in i2's initial
-   *     position, and vice versa). 
-   */
-  public void swap(T[] arr, int i1, int i2) {
-    // temporarily stores a value to swap with the other
-    T temp = null;
-
-    temp = arr[i2];
-    arr[i2] = arr[i1];
-    arr[i1] = temp;
-  } // swap(T, T)
-
+  } // mergeSort(T[])
 } // class MergeSorter
